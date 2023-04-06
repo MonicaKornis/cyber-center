63f675cd671f5cff3b289ddef6af9292e6856120
Monica Kornis <monicakornis@Monicas-MacBook-Pro-2.local>
2023-04-06 14:57:16 -0400

    Updates

---
+++ precache-manifest.73dc082f2b9d39dc763c8da71c005a31.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "692baddda02c7c953557",
+    "url": "/cyber-center/static/css/main.73bd3d95.chunk.css"
+  },
+  {
+    "revision": "692baddda02c7c953557",
+    "url": "/cyber-center/static/js/main.74afa605.chunk.js"
+  },
+  {
+    "revision": "4993f2cbb4d2bd58cc99",
+    "url": "/cyber-center/static/js/runtime~main.28312f08.js"
+  },
+  {
+    "revision": "e4df3a8be70a18076577",
+    "url": "/cyber-center/static/js/2.443535fd.chunk.js"
+  },
+  {
+    "revision": "85b67d58420062fbecec",
+    "url": "/cyber-center/static/js/3.eff08ea3.chunk.js"
+  },
+  {
+    "revision": "3cff10d100f20ff8e20d520ca082e228",
+    "url": "/cyber-center/static/media/homecomputer.3cff10d1.jpg"
+  },
+  {
+    "revision": "48f967bc5069ce96dd14c22e6f7175da",
+    "url": "/cyber-center/static/media/keyboard.48f967bc.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/cyber-center/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "29652b3a13180889e7bd3237312f57d2",
+    "url": "/cyber-center/static/media/main-2.29652b3a.jpg"
+  },
+  {
+    "revision": "3ebfe4cf6bad4ef8d76261484bf73c21",
+    "url": "/cyber-center/static/media/main.3ebfe4cf.jpg"
+  },
+  {
+    "revision": "1d65750159a1308701d440e60a3e2fbd",
+    "url": "/cyber-center/static/media/gameing.1d657501.jpg"
+  },
+  {
+    "revision": "86a7e8aa162c195baed4be33a4efa034",
+    "url": "/cyber-center/static/media/code.86a7e8aa.jpg"
+  },
+  {
+    "revision": "1bf904115b4dbb1fb74c94416763ec0d",
+    "url": "/cyber-center/static/media/networking.1bf90411.jpg"
+  },
+  {
+    "revision": "555bcbf45172acacd096380349d97025",
+    "url": "/cyber-center/static/media/logo.555bcbf4.svg"
+  },
+  {
+    "revision": "c9c5ba7ec93e8e0900efb24711b061c0",
+    "url": "/cyber-center/index.html"
+  }
+];
